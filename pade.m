3082f72f353f9e7d1a2db649d5e6b392fc97e622
Bastiaan Zapf <bzapf@online.de>
2011-10-11 22:44:51 +0200

    diverses ;)

--- pade.m
+++ pade.m
@@ -3,13 +3,13 @@ function [A,B]=pade(C,L,M)
   A=zeros(L,1);
   B=zeros(M,1);
   Cs = [];
-  for i=1:L    
-    Cs = [Cs;C(M+i:-1:i)];
+  for i=1:M
+    Cs = [Cs;C(L+i:-1:i)];
   endfor
   U=-Cs(:,1);
   V=Cs(:,2:end);
   B=V\U;
-  for j=1:L
+  for j=1:M
     A(j)=sum(C(j:-1:1).*[1;B(1:j-1)]');
   endfor
   
--- waveletphase.m
+++ waveletphase.m
@@ -4,16 +4,22 @@ function waveletphase(d)
   assert(d<0.55);
   X=sinc((-5:20)+d);
   [X,K,L]=wavelet(sinc(-10+d:503+d));
-#  plot(L);die();
   [B,A]=pade(L,7,7);  
-#  [b,a]=thiran(5+d,5);
-  U=shift(filter(B,[1;A],[1 zeros(1,256)]),-5);
-  V=shift(L,-5);
-  plot([U;V]');
+  U=filter(B,[1;A],[1 zeros(1,256)]); # apply pade approximant
+#  sound(U*5);
+#  U=[U(1:end) zeros(1,6)]; # shift and pad with zeros
+  V=L;
+  shift=d+128-4;
+  [X,X,W]=wavelet(H=sinc(-256+d:255+d));
+  size(W)
+  size(U) 
+  plot([U;V;W_=[W(round(shift):256) zeros(1,round(shift))]]');
+# U lacks some in the lowest frequencies
   pause;
 #  W=-(((1:128))*d*0.1*pi);
-  W=0:127;
-  W=((W.*d)/128).*pi;
+#  W=127:-1:0;
+#  W=(((W.*d)/128)-1).*pi;
   plot([angle(fft(U)(1:128));
 	angle(fft(V)(1:128));
-	-W]');
+	angle(fft(W_)(1:128))]');
+  
